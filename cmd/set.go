f1fd4d5e83edfa99ba33da4690a77e82272db87a
Mic-U <yuho.miki@gmail.com>
2021-03-07 09:59:35 +0900

    Lint check

--- cmd/set.go
+++ cmd/set.go
@@ -1,18 +1,4 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Package cmd defins each commands(get, set and so on)
 package cmd
 
 import (
@@ -23,6 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// SetOptions used in set commands
 type SetOptions struct {
 	Name string
 }
@@ -66,6 +53,10 @@ func init() {
 }
 
 func setCluster() {
-	config.EcsherConfigManager.SetCluster(setOptions.Name)
-	fmt.Printf("Cluster: %s\n", setOptions.Name)
+	err := config.EcsherConfigManager.SetCluster(setOptions.Name)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Cluster: %s\n", setOptions.Name)
+	}
 }
