c9eca7a13b3b6ad94332a3d9f43ae1eb4cfe6633
mic_u666 <yuho.miki@gmail.com>
2021-04-02 21:37:28 +0000

    Fix: exec command ignored --cluster option

--- cmd/exec.go
+++ cmd/exec.go
@@ -69,7 +69,7 @@ func init() {
 
 func exec(taskName string, command string) {
 	region := execOptions.Region
-	cluster := config.EcsherConfigManager.GetCluster(getOptions.Cluster)
+	cluster := config.EcsherConfigManager.GetCluster(execOptions.Cluster)
 	fmt.Printf("Cluster: %s\n", cluster)
 	client := ecs.GetClient(region)
 	output, err := ecs.ExecuteCommand(client, cluster, &ecs.ExecuteCmmandParams{
