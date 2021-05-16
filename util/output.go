c8dc759d0420c9ad1d588c367ad223c61a158da1
mic_u666 <yuho.miki@gmail.com>
2021-05-16 15:11:52 +0900

    Feature/output option (#52)
    * Add launch.json for debugging
    
    * Implements --output option for get command
    
    * update dependencies

---
+++ util/output.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"encoding/json"
+
+	"gopkg.in/yaml.v2"
+)
+
+var validOutputFormats []string = []string{"default", "yaml", "json"}
+
+func IsValidOutputFormat(outputFormat string) bool {
+	for _, v := range validOutputFormats {
+		if outputFormat == v {
+			return true
+		}
+	}
+	return false
+}
+
+func OutputAsYaml(input interface{}) (string, error) {
+	output, err := yaml.Marshal(input)
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func OutputAsJson(input interface{}) (string, error) {
+	output, err := json.MarshalIndent(input, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func IsYamlFormat(outputFormat string) bool {
+	return outputFormat == "yaml"
+}
+
+func IsJsonFormat(outputFormat string) bool {
+	return outputFormat == "json"
+}
